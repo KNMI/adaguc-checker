bf150c0acdb2cbb56d23d2160adad518b3b0e913
Ian van der Neut <neutvd@knmi.nl>
2018-03-06 15:22:22 +0100

    Added command line option to select check.
    Implemented place holders for adaguc checks. Call one of the
    place holders in _checker for demo/testing purposes.

--- adaguc-checker.py
+++ adaguc-checker.py
@@ -4,22 +4,43 @@ import os.path, argparse, sys
 from cfchecker import cfchecks
 
 class AdagucChecker(cfchecks.CFChecker):
-    def __init__(self):
+    def __init__(self, checks):
         cfchecks.CFChecker.__init__(self)
+        self.checks = checks.split(",")
 
     def _checker(self):
         print "Checking ADAGUC extensions"
-        cfchecks.CFChecker._checker(self)
+        if ("all" in self.checks or "adaguc" in self.checks):
+            self._check_dimensions()
+        if ("all" in self.checks or "standard" in self.checks):
+            cfchecks.CFChecker._checker(self)
+
+    def _check_latlon_bounds(self):
+        pass
+    def _check_latlon(self):
+        pass
+    def _check_time(self):
+        pass
+    def _check_variables(self):
+        pass
+    def _check_dimensions(self):
+        print "Dimensions:", self.f.dimensions.keys()
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Check NetCDF CF file for CF and ADAGUC compliance.")
     parser.add_argument("filename", help="Path to NetCDF file to check.")
+    parser.add_argument("--checks", type=str, dest="checks", nargs="?", action="store",
+                        default="all", required=False,
+                        help=("Checks to perform: all, adaguc, standard.\n"
+                              "all: perform both adaguc and standard cf."
+                              "adaguc: perform only adaguc related checks."
+                              "standard: perform only standard cf checks."))
 
     return parser.parse_args()
         
 def main():
     args = parse_args()
-    checker = AdagucChecker()
+    checker = AdagucChecker(args.checks)
     AdagucChecker.checker(checker, args.filename)
     sys.exit(0)
         
